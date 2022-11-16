dc810c7e6236ded0f6408b95969b554ccb201c8d
0viktory0 <36894469+0viktory0@users.noreply.github.com>
2022-11-16 14:02:51 +0300

    restoring order

--- scripts.py
+++ scripts.py
@@ -1,30 +1,34 @@
-from datacenter.models import Schoolkid, Mark, Chastisement, Commendation, Lesson, Subject
+from random import choice
+import sys
+
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
-import random
+
+from datacenter.models import Schoolkid, Mark, Chastisement, Commendation, Lesson
 
 
-COMMENDATIONS = ['Замечательно!',
-                'Здорово!',
-                'Очень хороший ответ!',
-                'Ты на верном пути!',
-                'Сказано здорово – просто и ясно!',
-                'Молодец!',
-                'Гораздо лучше, чем я ожидал!',
-                'Потрясающе!',
-                'Я вижу, как ты стараешься!',
-                'Ты растешь над собой!',
-                'Прекрасное начало!',
-                'Ты меня очень обрадовал!',
-                'С каждым разом у тебя получается всё лучше!',
-                'Ты растешь над собой!',
-                'Теперь у тебя точно все получится!',
-                'Ты многое сделал, я это вижу!',
-                'Я тобой горжусь!',
-                'Ты меня приятно удивил!',
-                'Хорошо!',
-                'Именно этого я давно ждал от тебя!',
-                'Великолепно!',
-                'Ты сегодня прыгнул выше головы!'
+COMMENDATIONS = [
+    'Замечательно!',
+    'Здорово!',
+    'Очень хороший ответ!',
+    'Ты на верном пути!',
+    'Сказано здорово – просто и ясно!',
+    'Молодец!',
+    'Гораздо лучше, чем я ожидал!',
+    'Потрясающе!',
+    'Я вижу, как ты стараешься!',
+    'Ты растешь над собой!',
+    'Прекрасное начало!',
+    'Ты меня очень обрадовал!',
+    'С каждым разом у тебя получается всё лучше!',
+    'Ты растешь над собой!',
+    'Теперь у тебя точно все получится!',
+    'Ты многое сделал, я это вижу!',
+    'Я тобой горжусь!',
+    'Ты меня приятно удивил!',
+    'Хорошо!',
+    'Именно этого я давно ждал от тебя!',
+    'Великолепно!',
+    'Ты сегодня прыгнул выше головы!'
 ]
 
 
@@ -32,17 +36,16 @@ def get_child_card(child_name):
     try:
         schoolkid = Schoolkid.objects.get(full_name__contains=child_name)
         return schoolkid
-    except ObjectDoesNotExist:
+    except Schoolkid.DoesNotExist:
         print(f'Ученика {child_name} нет в базе школы')
-    except MultipleObjectsReturned:
+    except Schoolkid.MultipleObjectsReturned:
         print(f'Найдено больше одного ученика с именем {child_name}')
 
 
 def fix_marks(schoolkid):
-    bad_marks = Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3])
-    for bad_mark in bad_marks:
-        bad_mark.points = 5
-        bad_mark.save()
+    Mark.objects.filter(
+        schoolkid=schoolkid,
+        points__in=[2, 3]).update(points=5)
 
 
 def delete_chastisements(schoolkid):
@@ -51,21 +54,22 @@ def delete_chastisements(schoolkid):
 
 
 def get_lesson(year_of_study, group_letter, subject):
-    lesson = Lesson.objects.filter(year_of_study=year_of_study,
-                                   group_letter=group_letter,
-                                   subject__title=subject).order_by('?').first()
+    lesson = Lesson.objects.filter(
+        year_of_study=year_of_study,
+        group_letter=group_letter,
+        subject__title=subject).order_by('?').first()
+    return lesson
     if lesson is None:
         print(f'Урок {subject} в базе не найден')
         sys.exit()
-    else:
-        return lesson
 
 
 def make_commendation(schoolkid, lesson):
-    text = random.choice(COMMENDATIONS)
-    Commendation.objects.create(text=text,
-                                created=lesson.date,
-                                schoolkid=schoolkid,
-                                subject=lesson.subject,
-                                teacher=lesson.teacher)
-
+    text = choice(COMMENDATIONS)
+    Commendation.objects.create(
+        text=text,
+        created=lesson.date,
+        schoolkid=schoolkid,
+        subject=lesson.subject,
+        teacher=lesson.teacher
+    )
